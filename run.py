770668b11a6cf67f850640bf7597fc6232795d23
David Pierre <davidpierrealves21@gmail.com>
2020-07-14 21:58:24 -0300

    Fix run.py to read project settings

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from doe_sangue.spiders.hemato import HematoSpider
 from doe_sangue.spiders.hemope import HemopeSpider
 from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+s = get_project_settings()
 
 if __name__ == "__main__":
-    process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    })
+    process = CrawlerProcess(s)
 
     process.crawl(HematoSpider)
     process.crawl(HemopeSpider)
