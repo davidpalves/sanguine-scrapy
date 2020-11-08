f9c9491d75ddb4959b4155de8753b7804dc72f7e
David Pierre <davidpierrealves21@gmail.com>
2020-11-08 15:34:46 +0000

    Add route to return all blood levels

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from decouple import config
 
 app = Flask(__name__)
+
+MONGODB_SERVER = config('MONGODB_SERVER')
+MONGODB_PORT = config('MONGODB_PORT')
+MONGODB_DB = config('MONGODB_DB')
+
+app.config["MONGO_URI"] = f"mongodb://{MONGODB_SERVER}:{MONGODB_PORT}/{MONGODB_DB}"
+
+mongo = PyMongo(app)
 
 from api import routes
--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,21 @@
-from api import app
+from api import app, mongo
+from flask import jsonify
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world"
+    niveis_sangue = mongo.db.niveis.find()
+
+    output = []
+    for nivel_sangue in niveis_sangue:
+        output.append({
+            'url': nivel_sangue['url'] or '', 
+            'banco': nivel_sangue['banco'] or '',
+            'data_extracao': nivel_sangue['data_extracao'] or '',
+            'endereco': nivel_sangue['endereco'] or '',
+            'cidade': nivel_sangue['cidade'] or '',
+            'sangue': nivel_sangue['sangue'] or '',
+        })
+
+    return jsonify(output)
 
