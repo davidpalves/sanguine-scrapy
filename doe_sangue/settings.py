8478890d4beca40e762738c6865a4499f186bc3c
David Pierre <davidpierrealves21@gmail.com>
2021-10-11 22:15:11 -0300

    Fix shellscript to run spiders

--- doe_sangue/settings.py
+++ doe_sangue/settings.py
@@ -5,7 +5,7 @@ BOT_NAME = 'doe_sangue'
 SPIDER_MODULES = ['doe_sangue.spiders']
 NEWSPIDER_MODULE = 'doe_sangue.spiders'
 
-LOG_LEVEL = 'INFO'
+LOG_LEVEL = 'ERROR'
 
 
 # Crawl responsibly by identifying yourself (and your website)
--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd doe_sangue
+cd doe_sangue || exit
 
-JUMPLINE="\n\n"
+JUMPLINE=""
 
-IFS=$'\n' spiders=($(scrapy list))
+mapfile -t spiders < <(scrapy list)
 
 echo "================= STARTING SCRAPING ================="
 
-for i in $spiders; do
+for i in "${spiders[@]}"; do
     echo "${JUMPLINE}"
     echo "Spider ${i}"
     echo "Crawling Spider ${i}..."
-    scrapy crawl $i
+    scrapy crawl "${i}"
     echo "Spider ${i} finished scraping"
 done
