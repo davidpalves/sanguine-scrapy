c6a816ab67e63c7ce2130818192ff9a4a57bb9cd
David Alves <davidpierrealves21@gmail.com>
2022-01-02 02:24:36 -0300

    Add filters to Blood api listing

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,6 @@
+import re
 from api import app, mongo
-from flask import jsonify, Response
+from flask import jsonify, request
 import subprocess
 
 
@@ -8,15 +9,26 @@ import subprocess
 def index():
     niveis_sangue = mongo.db.niveis
 
+    cidade = request.args.get('cidade')
+    estado = request.args.get('estado')
+    banco = request.args.get('banco')
+
+    filter = {}
+    if cidade: filter['cidade'] = re.compile('^' + re.escape(cidade) + '$', re.IGNORECASE)
+    if estado: filter['estado'] = re.compile('^' + re.escape(estado) + '$', re.IGNORECASE)
+    if banco: filter['banco'] = re.compile('^' + re.escape(banco) + '$', re.IGNORECASE)
+
     output = []
-    for nivel_sangue in niveis_sangue.find():
+    for nivel_sangue in niveis_sangue.find(filter):
         output.append({
-            'url': nivel_sangue['url'],
-            'banco': nivel_sangue['banco'],
-            'data_extracao': nivel_sangue['data_extracao'],
-            'endereco': nivel_sangue['endereco'],
-            'cidade': nivel_sangue['cidade'],
-            'sangue': nivel_sangue['sangue'],
+            'url': nivel_sangue.get('url'),
+            'banco': nivel_sangue.get('banco'),
+            'data_extracao': nivel_sangue.get('data_extracao'),
+            'endereco': nivel_sangue.get('endereco'),
+            'cidade': nivel_sangue.get('cidade'),
+            'estado': nivel_sangue.get('estado'),
+            'unidade': nivel_sangue.get('unidade'),
+            'sangue': nivel_sangue.get('sangue'),
         })
 
     return jsonify(output), 200
