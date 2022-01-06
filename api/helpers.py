53a7922be94d3ea2a245d846837ffefed01e40f8
David Alves <davidpierrealves21@gmail.com>
2022-01-06 10:15:37 -0300

    Remove debugging lines

--- api/helpers.py
+++ api/helpers.py
@@ -4,8 +4,7 @@ from run import execute_spiders
 def force_data_update():
     message = 'All data was updated'
     try:
-        test = execute_spiders()
-        print(test)
+        execute_spiders()
     except Exception:
         message = 'Could not fetch data properly'
         return jsonify({'details': message}), 400
