fe2c6ef68b9328f73eda87c3edc494d8a4c00aac
David Alves <davidpierrealves21@gmail.com>
2022-01-06 10:15:06 -0300

    Remove unused code block

--- api/helpers.py
+++ api/helpers.py
@@ -1,16 +1,5 @@
 from flask import jsonify
 from run import execute_spiders
-import subprocess
-
-# def update_data():
-#     script_command = 'python ../run.py'
-#     message = 'All data was updated'
-#     try:
-#         subprocess.Popen(script_command, stdout=subprocess.PIPE)
-#     except Exception:
-#         message = 'Could not fetch data properly'
-#         return jsonify({'details': message}), 400
-#     return jsonify({'details': message}), 200
 
 def force_data_update():
     message = 'All data was updated'
