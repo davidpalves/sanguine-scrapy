06675453128fe14dccc87a7cc4b8b3ed3fdf52fe
Mateus Fittipaldi <mateusfitti@gmail.com>
2022-05-27 00:00:02 -0300

    Create user registration endpoint and resource

--- api/app.py
+++ api/app.py
@@ -6,8 +6,10 @@ from api.common.commands import update_data_bp
 
 from api.resources.blood_level_list import BloodLevelsList
 from api.resources.blood_banks_list import BloodBanksList
+from api.resources.users import CreateUser
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://mongodb:27017/doe_sangue"
 api = Api(app)
 
 app.register_blueprint(update_data_bp)
@@ -21,3 +23,4 @@ mongo.init_app(app)
 
 api.add_resource(BloodLevelsList, '/')
 api.add_resource(BloodBanksList, '/bancos-cadastrados/')
+api.add_resource(CreateUser, '/create-user/')
